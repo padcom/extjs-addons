4308bdfc35cd360243d3e978de4e735f86b1cc59
Matthias Hryniszak <padcom@gmail.com>
2011-03-13 15:03:35 +0100

    Code cleanup (indentation)

--- ux/grid/AjaxRowExpander.js
+++ ux/grid/AjaxRowExpander.js
@@ -13,29 +13,29 @@
 * @ptype ajaxrowexpander
 */
 Ext.ux.grid.AjaxRowExpander = Ext.extend(Ext.ux.grid.RowExpander, {
-	/**
- 	* @cfg {String} mode
-	* <p>The mode in which to render the content</p>
-	* <dl>
-	* <dt>html</dt>
-	* <dd>The content returned by server is inserted as-is into the body element</dd>
-	* <dt>tpl</dt>
-	* <dd>The content returned by server is transformed using the tpl and inserted into the body element. In this case
-	*     there are 2 properties that you can use in the template:
-	*     <ul>
-	*        <li>parent - which points to the parent record</li>
-	*        <li>details - which points to the object returned by server</li>
-	*     </ul>
-	* </dd>
-	* </dl>
-	* <p>Defaults to 'html'</p>
- 	*/
+    /**
+     * @cfg {String} mode
+    * <p>The mode in which to render the content</p>
+    * <dl>
+    * <dt>html</dt>
+    * <dd>The content returned by server is inserted as-is into the body element</dd>
+    * <dt>tpl</dt>
+    * <dd>The content returned by server is transformed using the tpl and inserted into the body element. In this case
+    *     there are 2 properties that you can use in the template:
+    *     <ul>
+    *        <li>parent - which points to the parent record</li>
+    *        <li>details - which points to the object returned by server</li>
+    *     </ul>
+    * </dd>
+    * </dl>
+    * <p>Defaults to 'html'</p>
+     */
     mode: 'html', 
 
-	/**
- 	* @cfg {String} loadingIndicatorTpl
-	* <p>The template to use while the content is loading. Default is '<div id="{id}">Loading...<div>'</p>
- 	*/
+    /**
+     * @cfg {String} loadingIndicatorTpl
+    * <p>The template to use while the content is loading. Default is '<div id="{id}">Loading...<div>'</p>
+     */
     loadingIndicatorTpl: new Ext.Template('<div id="{id}">Loading...<div>'),
 
     getBodyContent : function(record, index) {
